e7177f61749ba68d19cbfdbda50dba17b26efdfd
Jeremy Magland <jmagland@flatironinstitute.org>
2025-10-02 12:07:51 -0400

    fix spacing

--- src/assistants/stan-assistant/preferences.tsx
+++ src/assistants/stan-assistant/preferences.tsx
@@ -8,18 +8,29 @@ const AssistantDisplayInfo = () => {
   const stanUrl = "https://mc-stan.org/";
   const stanUsersGuideUrl =
     "https://mc-stan.org/docs/stan-users-guide/index.html";
-  const stanReferenceManualUrl =
-    "https://mc-stan.org/docs/reference-manual/";
+  const stanReferenceManualUrl = "https://mc-stan.org/docs/reference-manual/";
   const docPages = useMemo(() => getDocPages(), []);
   return (
     <div style={{ maxWidth: "600px", margin: "0 auto" }}>
       <p>
-          This is a technical assistant for <a href={stanUrl} target="_blank" rel="noopener noreferrer">Stan</a> that provides
-          guidance, troubleshooting, and explanations based on the official{" "}
-          <a href={stanUsersGuideUrl} target="_blank" rel="noopener noreferrer">
-            Stan User's Guide
-          </a>
-          and a condensed version of the <a href={stanReferenceManualUrl} target="_blank" rel="noopener noreferrer">Stan Reference Manual</a>.
+        This is a technical assistant for{" "}
+        <a href={stanUrl} target="_blank" rel="noopener noreferrer">
+          Stan
+        </a>{" "}
+        that provides guidance, troubleshooting, and explanations based on the
+        official{" "}
+        <a href={stanUsersGuideUrl} target="_blank" rel="noopener noreferrer">
+          Stan User's Guide
+        </a>{" "}
+        and a condensed version of the{" "}
+        <a
+          href={stanReferenceManualUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Stan Reference Manual
+        </a>
+        .
       </p>
       <p>
         {docPages.map((doc, index) => (
@@ -39,7 +50,11 @@ const AssistantDisplayInfo = () => {
 const preferences: Preferences = {
   assistantSystemPrompt: stanAssistantSystemPrompt,
   assistantDisplayInfo: <AssistantDisplayInfo />,
-  suggestedPrompts: ["Give an overview of Stan's modeling language.", "Provide an example Stan model for linear regression.", "Provide an interactive example using Stan Playground."],
+  suggestedPrompts: [
+    "Give an overview of Stan's modeling language.",
+    "Provide an example Stan model for linear regression.",
+    "Provide an interactive example using Stan Playground.",
+  ],
 };
 
 export default preferences;
